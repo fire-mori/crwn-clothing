f39d7e2194fc8cd4c5fee288dff4ff5f00e24a8a
Kseniya Rudenko <rudenkokseniya91@gmail.com>
2021-07-11 13:44:19 +1200

    upadate menu-items to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/hompage.component';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route component={HomePage} path="/" exact />
+        <Route component={HatsPage} path="/hats" />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,30 +12,35 @@ class Directory extends React.Component {
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           size: 'default',
           id: 1,
+          linkUrl: 'hats',
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           size: 'default',
           id: 2,
+          linkUrl: '',
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           size: 'default',
           id: 3,
+          linkUrl: '',
         },
         {
           title: 'women',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
+          linkUrl: '',
         },
         {
           title: 'men',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
+          linkUrl: '',
         },
       ],
     };
@@ -46,9 +51,9 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {sections.map(({
-          title, imageUrl, id, size,
+          id, ...sectionsProps
         }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          <MenuItem key={id} {...sectionsProps} />
         ))}
       </div>
     );
