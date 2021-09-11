39f837cb599063e60d86b85732fb9dbc388c0c05
Kseniya Rudenko <rudenkokseniya91@gmail.com>
2021-09-12 10:54:12 +1200

    add checkout page, checkout item component, update cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import setCurrentUser from './redux/user/user.actions';
+import selectCurrentUser from './redux/user/user.selectors';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -42,6 +45,7 @@ class App extends React.Component {
         <Switch>
           <Route component={HomePage} path="/" exact />
           <Route component={ShopPage} path="/shop" />
+          <Route component={CheckoutPage} path="/checkout" exact />
           <Route
             path="/signin"
             component={Component}
@@ -53,8 +57,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -11,3 +11,16 @@ export const selectCartItemsCount = createSelector(
     0,
   ),
 );
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden,
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce(
+    (accumulator, cartItem) => accumulator + cartItem.quantity * cartItem.price,
+    0,
+  ),
+);
