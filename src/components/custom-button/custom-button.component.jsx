5d2ab5dff7acc1051bc15d268a5a6c4fcd3bd8c6
Kseniya Rudenko <rudenkokseniya91@gmail.com>
2021-10-25 13:22:29 +1300

    add styled-components, convert homepage, header and button to styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import './custom-button.styles.scss';
+import CustomButtonContainer from './custom-button.styles';
 
-const CustomButton = ({
-  children, inverted, isGoogleSignIn, ...buttonProps
-}) => (
-  <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} ${
-      inverted ? 'inverted' : ''
-    } custom-button`}
-    {...buttonProps}
-  >
-    {children}
-  </button>
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
